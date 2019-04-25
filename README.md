9c1f3ae98924d9f3390ef8424c0233d57426d9d7
yaswanthreddy4 <yaswanthreddy4@gmail.com>
2019-04-25 07:22:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material2-issue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material2-issue)
---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import {
+  Component, ElementRef
+} from '@angular/core';
+import { VERSION } from '@angular/material';
+
+@Component({
+  selector: 'material-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  version = VERSION;
+}
+
+/**
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
